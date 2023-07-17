6d378bb1460b6592b907038384414d6b773bc7da
NavjotRuler <navjot.kumar.e21@nsut.ac.in>
2023-07-17 16:46:09 +0530

    longest word

---
+++ LongestWord.js
@@ -0,0 +1,16 @@
+const longestWord = (str) => {
+let arr = str.split(" ");
+let max = arr[0];
+
+for(let i = 1; i < arr.length; i++){
+    if(max.length<arr[i].length) {
+        max = arr[i];
+        // console.log(arr[i], max);
+    }
+}
+return max;
+}
+
+// longestWord('My name is Vishwas') - Returns 'Vishwas'
+const output = longestWord('Hello world');
+console.log(output);
